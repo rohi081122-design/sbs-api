3483ae88bb9157670e109a2cf5ac6a1462f562f8
surag <rohi081122@gmail.com>
2026-04-16 08:05:01 +0530

    add extra layer token

--- Routes/contact.js
+++ Routes/contact.js
@@ -1,110 +1,174 @@
+require('dotenv').config();
 const express = require('express')
 const Router = express.Router()
 const Contact = require('../models/Contact')
+const jwt = require('jsonwebtoken')
 
-Router.post('/add-contact', (req, res) => {
-    //    console.log(req.body)
-    const newContact = new Contact({
-        fullName: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        address: req.body.address,
-        gender: req.body.gender
-    })
+// add contact
 
-    newContact.save()
-        .then((result) => {
-            console.log('data saved')
-            res.status(200).json({
-                msg: 'data saved'
-            })
+// Router.post('/add-contact',(req,res)=>{
+//     const newContact = new Contact({
+//         fullName:req.body.name,
+//         email:req.body.person_email,
+//         phone:req.body.person_phone,
+//         address:req.body.add
+//     })
+
+//     newContact.save()
+//     .then((newData)=>{
+//         console.log('data saved')
+//         res.status(200).json({
+//            result:newData
+//         })
+//     })
+//     .catch((err)=>{
+//         console.log(err)
+//         res.status(500).json({
+//             error:err
+//         })
+//     })
+// })
+
+
+Router.post('/add-contact',async(req,res)=>{
+    try {
+        // console.log(req.headers.authorization.split(" ")[1])
+        const token = req.headers.authorization.split(" ")[1]
+        const tokenData = await jwt.verify(token,process.env.SEC_KEY)
+        console.log(tokenData)
+        const newContact = new Contact({
+            fullName: req.body.fullName,
+            email: req.body.email,
+            phone: req.body.phone,
+            address: req.body.address,
+            gender:req.body.gender,
+            userId:tokenData.userId
         })
-        .catch((err) => {
-            console.log(err)
-            res.status(500).json({
-                error: 'something is wrong'
-            })
+        
+        const newData = await newContact.save()
+        res.status(200).json({
+            result:newData
         })
 
+    }
+    catch(err)
+    {
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    }
 })
 
 // get all contact
-Router.get('/all-contact', async (req, res) => {
-    try {
-        const allContact = await Contact.find()
+Router.get('/all-contact',async(req,res)=>{
+    try
+    {
+        const token = req.headers.authorization.split(" ")[1]
+        const tokenData = await jwt.verify(token,process.env.SEC_KEY)
+        const allContact = await Contact.find({userId:tokenData.userId}).select("_id fullName email phone address gender userId")
         res.status(200).json({
-            contacts: allContact
+            contacts:allContact
         })
     }
-    catch (err) {
+    catch(err)
+    {
         console.log(err)
         res.status(500).json({
-            error: err
+            error:err
         })
     }
 })
 
+
 // get contact by id
-Router.get('/contactById/:id', async (req, res) => {
-    try {
+Router.get('/contactById/:id',async(req,res)=>{
+    try
+    {
+        // console.log(req.params.id)
+        const token = req.headers.authorization.split(" ")[1]
+        const tokenData = await jwt.verify(token,process.env.SEC_KEY)
         const id = req.params.id
-        const data = await Contact.findById(id).select("_id fullName email phone address gender")
-        res.status(200).json({
-            contact: data
+        // const data = await Contact.findById(id).select("_id fullName email phone address gender userId")
+        const data = await Contact.find({_id:req.params.id,userId:tokenData.userId})
+        return res.status(200).json({
+            contact:data.length>0 ? data[0] : {}
         })
+        
+
+
     }
-    catch (err) {
+    catch(err)
+    {
         console.log(err)
         res.status(500).json({
-            error: err
+            error:err
         })
     }
 })
-// Gender contact by gender
-Router.get('/gender/:g', async (req, res) => {
-    try {
-        const contact = await Contact.find({ gender: req.params.g })
+
+// get contact by gender
+Router.get('/gender/:g',async(req,res)=>{
+    try
+    {
+        const token = req.headers.authorization.split(" ")[1]
+        const tokenData = await jwt.verify(token,process.env.SEC_KEY)
+        const contact = await Contact.find({gender:req.params.g,userId:tokenData.userId})
         res.status(200).json({
-            contact: contact
+            contact:contact
         })
     }
-    catch (err) {
+    catch(err)
+    {
         console.log(err)
         res.status(500).json({
-            error: err
+            error:err
         })
     }
 })
-//delete by id
-Router.delete('/:id', async (req, res) => {
-    try {
-        await Contact.deleteOne({ _id: req.params.id })
+
+// delete api 
+Router.delete('/:id',async(req,res)=>{
+    try
+    {
+        const token = req.headers.authorization.split(" ")[1]
+        const tokenData = await jwt.verify(token,process.env.SEC_KEY)
+        await Contact.deleteOne({_id:req.params.id,userId:tokenData.userId})
         res.status(200).json({
-            msg: 'data deleted'
+            msg:'data deleted'
         })
     }
-    catch (err) {
+    catch(err)
+    {
         console.log(err)
         res.status(500).json({
-            error: err
+            error:err
         })
     }
 })
-//delete many data
-Router.delete('/bygender/:gender', async (req, res) => {
-    try {
-        await Contact.deleteMany({ gender: req.params.gender })
+
+// delete many data
+Router.delete('/byGender/:gender',async(req,res)=>{
+    try
+    {
+        const token = req.headers.authorization.split(" ")[1]
+        const tokenData = await jwt.verify(token,process.env.SEC_KEY)
+        await Contact.deleteMany({gender:req.params.gender,userId:tokenData.userId})
         res.status(200).json({
-            msg: 'all data deleted by this genden......'
+            msg:'all contact of this gender deleted....'
         })
     }
-    catch (err) {
+    catch(err)
+    {
         console.log(err)
         res.status(500).json({
-            error: err
+            error:err
         })
     }
 })
+
+
+
 
 
 module.exports = Router
